9497b8c743a318aa8d324a85bf3d5db644f6f4cc
tushal kumar <tushalkashyap11@gmail.com>
2021-07-02 04:52:39 -0700

    Added : Code to use sum of three number print 0

--- ArrayJS/4sumOfThree.js
+++ ArrayJS/4sumOfThree.js
@@ -3,10 +3,10 @@ const prompt = require("prompt-sync")();
     let numberArr = [];
     let numberOne = Number(prompt("Enter first number "));
     let numberTwo = Number(prompt("Enter second number "));
-    let numberThree = Number(prompt("Enter Third number "));
+    //let numberThree = Number(prompt("Enter Third number "));
     numberArr.push(numberOne);
     numberArr.push(numberTwo);
-    numberArr.push(numberThree);
+    //numberArr.push(numberThree);
 
     function sum(total, num) {
         return total + num;
